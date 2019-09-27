f52cdfd85dd3f51e92080c489b9cfc99dc65be5e
Jazmine Alfaro <jazmine.alfaro@ucsp.edu.pe>
2019-09-27 13:30:02 -0500

    tetera

--- main.cpp
+++ main.cpp
@@ -85,24 +85,23 @@ void glPaint(void) {
 	//El fondo de la escena al color initial
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 	glLoadIdentity();
-	glOrtho(-300.0f,  300.0f,-300.0f, 300.0f, -1.0f, 1.0f);
-		
+	//glOrtho(-300.0f,  300.0f,-300.0f, 300.0f, -1.0f, 1.0f);
+	/// Esto fue lo que cambiÃ© jijiji
+	gluPerspective(45.0, 1.0, 1.0, 500.0);
+	
+	
 	glPolygonMode(GL_FRONT, GL_LINE);
 	glPolygonMode (GL_BACK, GL_LINE);
-	//glPointSize(3);
 	
-	//int n = read(fd, &e, sizeof(e));
-	
-	//printf("%d %d %d %d\n", e.value, e.number, e.type, e.time);
-	
-	glBegin(GL_POLYGON_MODE);
-	
-	glColor3d(0,0,255);
+	glTranslatef(0, 0, -300.0);
+	glPushMatrix();
+	glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
 	glTranslatef(particle->x, particle->y, 0.0f);
-	glRotatef(a, 0.0f, 1.0f, 0.0f);
-	glutSolidTeapot(50);
+	glColor3d(1.0f, 0.0f, 1.0f);	
+	glutSolidTeapot(10);
 	
-	glEnd();
+	glPopMatrix();
+	
 	
 	//particle->draw();
 	
@@ -136,15 +135,17 @@ void draw_point(int x, int y){
 //
 void init_GL(void) {
 	//Color del fondo de la escena
-	glClearColor(0, 0, 0, 0.0f); //(R, G, B, transparencia) en este caso un fondo negro
-	glEnable(GL_COLOR_MATERIAL);
+	//glClearColor(0, 0, 0, 0.0f); //(R, G, B, transparencia) en este caso un fondo negro
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//glEnable(GL_COLOR_MATERIAL);
 	//modo projeccion
-	glMatrixMode(GL_PROJECTION);
+	//glMatrixMode(GL_PROJECTION);
 	
 	glLoadIdentity();
+	glutSwapBuffers();
 }
 
-//en el caso que la ventana cambie de tamaño
+//en el caso que la ventana cambie de tamaÃ±o
 GLvoid window_redraw(GLsizei width, GLsizei height) {
 	glViewport(0, 0, width, height);
 	glMatrixMode(GL_PROJECTION);
@@ -166,23 +167,13 @@ GLvoid window_key(unsigned char key, int x, int y) {
 GLvoid initGL()
 {
 	GLfloat position[] = { 0.0f, 5.0f, 10.0f, 0.0 };
-	
-	//enable light : try without it
-	glLightfv(GL_LIGHT0, GL_POSITION, position);
-	glEnable(GL_LIGHTING);
-	//light 0 "on": try without it
-	glEnable(GL_LIGHT0);
-	
-	//shading model : try GL_FLAT
-	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	
 	glEnable(GL_DEPTH_TEST);
 	
-	//enable material : try without it
-	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-	glEnable(GL_COLOR_MATERIAL);
-	
-	glClearColor(RED, GREEN, BLUE, ALPHA);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
 }
 //
 //el programa principal
@@ -196,7 +187,7 @@ int main(int argc, char** argv) {
 	//Inicializacion de la GLUT
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(600, 600); //tamaño de la ventana
+	glutInitWindowSize(800, 800); //tamaÃ±o de la ventana
 	glutInitWindowPosition(100, 100); //posicion de la ventana
 	glutCreateWindow("Chicken Invaders"); //titulo de la ventana
 	
